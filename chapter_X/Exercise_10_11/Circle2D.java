b79ef679992e49e4fa3e5fbe23bc99676a049c50
StanislawTobolewski <stanislaw.tobolewski@gmail.com>
2022-11-07 22:40:58 +0100

    update

--- chapter_X/Exercise_10_11/Circle2D.java
+++ chapter_X/Exercise_10_11/Circle2D.java
@@ -4,12 +4,55 @@ public class Circle2D {
 
     private double x;
     private double y;
+    private double radius;
 
-    public Circle2D(){}
+    public Circle2D(){
+        x = 0;
+        y = 0;
+        radius = 1;
+    }
 
     public Circle2D(int x, int y){
         this.x = x;
         this.y = y;
+    }
+    public Circle2D(double x, double y, double r){
+        this.x = x;
+        this.y = y;
+        this.radius = r;
+    }
+
+    public double getArea(){
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    public double getPerimeter(){
+        return (2 * Math.PI * radius);
+    }
+
+    //d=√((x_2-x_1)²+(y_2-y_1)²
+    public boolean contains(double x, double y){
+        double distance = 0.0;
+        double left = Math.pow((x - this.x),2);
+        double right = Math.pow((y - this.y),2);
+        distance = Math.pow((left + right),0.5);
+        return (distance<this.radius)?true:false;
+    }
+
+    public boolean contains(Circle2D circle){
+        double distance = 0.0;
+        double left = Math.pow((circle.x - this.x),2);
+        double right = Math.pow((circle.y - this.y),2);
+        distance = Math.pow((left + right),0.5);
+        return (distance + circle.radius<this.radius)?true:false;
+    }
+
+    public boolean overlaps(Circle2D circle){
+        double distance = 0.0;
+        double left = Math.pow((circle.x - this.x),2);
+        double right = Math.pow((circle.y - this.y),2);
+        distance = Math.pow((left + right),0.5);
+        return (distance < circle.radius+this.radius)?true:false;
     }
 
     public double getX() {
--- chapter_X/Exercise_10_11/Main.java
+++ chapter_X/Exercise_10_11/Main.java
@@ -1,5 +1,11 @@
 package chapter_X.Exercise_10_11;
 
 public class Main {
+    public static void main(String[] args) {
+        Circle2D c1 = new Circle2D(0,0,3.0);
+        Circle2D c2 = new Circle2D(0,6,4.0);
 
+        System.out.println(c1.overlaps(c2));
+
+    }
 }
